f10b5fd8caaf57fc37e44566e066bd593e022528
Bipin Krishnan <bipinkris@gmail.com>
2022-08-14 08:56:03 +0530

    changes done in new branch

--- file1.cpp
+++ file1.cpp
@@ -2,4 +2,4 @@ This is the first file in repository at remote main branch
   First file is modified at 5 PM 13th Aug at remote main branch
   First file is modified at 5.15 PM 13th Aug at local main branch
   First file is modified at 5.30 PM 13th Aug at remote main branch
-  
+  Changed in new-branch-git-trial
