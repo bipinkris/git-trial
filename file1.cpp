5f243e70ef05d3ce9fa409738e0fadde83d01e96
Bipin Krishnan <bipinkris@gmail.com>
2022-08-14 15:52:20 +0530

    bug fix

--- file1.cpp
+++ file1.cpp
@@ -1,5 +1,5 @@
 This is the first file in repository at remote main branch
   First file is modified at 5 PM 13th Aug at remote main branch
   First file is modified at 5.15 PM 13th Aug at local main branch
-  First file is modified at 5.30 PM 13th Aug at remote main branch
+  This file is modified at 5.30 PM 13th Aug at remote main branch
   Updated in main branch
