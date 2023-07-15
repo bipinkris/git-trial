30ec58417b346b85afca4cdaeb376376e208363f
Bipin Krishnan <bipinkris@gmail.com>
2023-07-15 08:39:56 +0530

    before pushing changes

--- userprog.c
+++ userprog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hello World latest git changes\n");
+	//printf("Hello World latest git changes\n");
+	printf("Hello World before pushing changes\n");
 	return 0;
 }
